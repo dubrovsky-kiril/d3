16af54e7b4d88965baed57eb49d33db2ed0b816d
Kirils Golubevs <kirils.golubevs@lv-wks-25.wandoo.eu>
2019-08-11 15:04:46 +0300

    WIP

--- src/components/Graphics/ProbabilityDistribution.tsx
+++ src/components/Graphics/ProbabilityDistribution.tsx
@@ -143,6 +143,101 @@ const ProbabilityDistribution: React.FC<IProbabilityDistribution> = ({
       .append("stop")
       .attr("offset", d => d.offset)
       .attr("stop-color", d => d.color);
+
+    let blueTreshold = 0.15;
+    const draggingRectangleHeight = 30;
+    const draggingRectangleWidth = 5;
+    const tresholdLineWidth = 1;
+
+    // Treshold rectangle
+    area
+      .append("g")
+      .attr("id", "treshold-rect")
+      .append("rect")
+      .attr("id", "blue-treshold-rect")
+      .attr("height", dimensions.height)
+      .attr("fill", "blue")
+      .attr("width", xScale(blueTreshold))
+      .style("opacity", "0.1");
+
+    // Treshold line
+    d3.select("#treshold-rect")
+      .append("line")
+      .attr("id", "treshold-line")
+      .attr("x1", xScale(blueTreshold) + tresholdLineWidth / 2)
+      .attr("y1", "0%")
+      .attr("x2", xScale(blueTreshold) + tresholdLineWidth / 2)
+      .attr("y2", dimensions.height)
+      .attr("stroke", "blue")
+      .attr("stroke-width", 1)
+      .style("opacity", 0.5);
+
+    function redraw() {
+      // Remove possability to drag rectan`gle to negative X coordinates
+      d3.select("#dragging-rectangle")
+        .attr("x", blueTreshold >= 0 ? blueTreshold : 0)
+        .attr("transform", `translate(-2, 0)`);
+
+      d3.select("#dragging-text")
+        .text(
+          +xScale.invert(blueTreshold).toFixed(3) > 0
+            ? +xScale.invert(blueTreshold).toFixed(3)
+            : "0.00"
+        )
+        .attr("x", () => {
+          if (xScale.invert(blueTreshold).toFixed(3).length > 3) {
+            return blueTreshold >= 0 ? blueTreshold - 41 : -41;
+          } else {
+            return blueTreshold >= 0 ? blueTreshold - 41 : -41;
+          }
+        });
+
+      d3.select("#treshold-line")
+        .attr(
+          "x1",
+          blueTreshold >= 0 ? blueTreshold + draggingRectangleWidth / 2 : 2.5
+        )
+        .attr(
+          "x2",
+          blueTreshold >= 0 ? blueTreshold + draggingRectangleWidth / 2 : 2.5
+        )
+        .attr("transform", `translate(-2, 0)`);
+
+      d3.select("#blue-treshold-rect").attr(
+        "width",
+        blueTreshold >= 0 ? blueTreshold + tresholdLineWidth : 0
+      );
+    }
+
+    function on_drag() {
+      blueTreshold = d3.event.x;
+      redraw();
+    }
+
+    // Dragging rectangle
+    d3.select("#treshold-rect")
+      .append("rect")
+      .attr("id", "dragging-rectangle")
+      .attr("fill", "blue")
+      .attr("height", draggingRectangleHeight)
+      .attr("width", draggingRectangleWidth)
+      .attr("x", xScale(blueTreshold))
+      .attr("transform", `translate(-2, 0)`)
+      .attr("y", (dimensions.height - draggingRectangleHeight) / 2)
+      .attr("rx", 2)
+      .attr("ry", 2)
+      .style("cursor", "ew-resize")
+      .call(d3.drag().on("drag", on_drag));
+
+    // Dragging rectangle text
+    d3.select("#treshold-rect")
+      .append("text")
+      .attr("id", "dragging-text")
+      .text(blueTreshold)
+      .attr("x", xScale(blueTreshold) - 40)
+      .attr("y", (dimensions.height + draggingRectangleWidth) / 2)
+      .style("font-size", "14px")
+      .style("fill", "blue");
   };
 
   React.useEffect(() => {
